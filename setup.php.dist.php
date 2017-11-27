a126b385de4c46a101fb1d91e11e7b3c93ddfdaf
Tad <tad0616@gmail.com>
2017-11-27 22:51:20 +0800

    將config.php的設定也移到setup.php統一設定

--- setup.php.dist.php
+++ setup.php.dist.php
@@ -9,3 +9,15 @@ define('_DB_USER', 'root');
 define('_DB_PASS', '');
 //資料庫名稱
 define('_DB_NAME', 'street_news');
+//用來驗證使用者的信箱（不填代表由使用者自行驗證）
+define('_VERIFY_EMAIL', '');
+//管理員信箱
+define('_ADM_EMAIL', '');
+//是否使用SMTP寄信？
+define('_USE_SMTP', false);
+//SMTP 主機
+define('_SMTP_SERVER', 'smtp.gmail.com');
+//SMTP 帳號，如： xxx@gmail.com
+define('_SMTP_USER', '');
+//SMTP 密碼，如： xxx@gmail.com的密碼
+define('_SMTP_PASS', '');

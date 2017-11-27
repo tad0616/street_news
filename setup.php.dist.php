f8a7618c5991284b0a3e8932630f1f1cc046cc34
Tad <tad0616@gmail.com>
2017-11-27 21:08:06 +0800

    新增設定檔 setup.php

---
+++ setup.php.dist.php
@@ -0,0 +1,11 @@
+<?php
+//請將此檔更名為 setup.php，並填好相關設定值
+
+//資料庫位址
+define('_DB_HOST', 'localhost');
+//資料庫使用者
+define('_DB_USER', 'root');
+//資料庫密碼
+define('_DB_PASS', '');
+//資料庫名稱
+define('_DB_NAME', 'street_news');
